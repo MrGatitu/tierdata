3c452645074a5c332b4bf7482eae261b2151f52f
beatle <davidmurichuz2@gmail.com>
2024-07-18 23:53:09 -1000

    second commit

--- hck/my-app/src/components/pages/index.jsx
+++ hck/my-app/src/components/pages/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Home from '../Dashboard/LandingPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../Admin/login';
 
 const Pages = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
